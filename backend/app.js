b4470b446eb97a0753f78c036ff77b0688b40d38
J0z3Hp <joseluismarinocuadros@gmail.com>
2024-10-25 23:47:35 -0500

    [creation] Proyecto final

---
+++ backend/app.js
@@ -0,0 +1,18 @@
+// El archivo app.js -> es el archivo de ejecucion de nustro aplicativo web
+// Acá vamos a configurar nuestro servido con EXPRESS y vamos a gestionar todo lo relacioanado con la logica de negocio -> conexion base de datos, peticiones, respuestas
+
+// 1. IMPORTAR LAS DEPENDENCIAS Y MODULOS QUE NECESITAMOS, PARA TODAS LA DEPENCIAS QUE QUEREMOS USAR SIEMPRE TENEMOS QUE IMPORTARLAS 
+import express from "express"; // ECMAS 6
+import dotenv from "dotenv"; // Dependencia para manjar variable de entorno
+import { connectionMongo } from "./src/config/dataBase.js"; // siempre agragar el .js al archivo 
+
+// 2. Configurar el uso de nuestro servidor 
+const app = express();
+dotenv.config(); // Configurando para poder usar variable de entorno
+connectionMongo (); // Estamos invocando nuestra importacion "connectionMongo"
+const port = process.env.PORT // 3000, 6000, 9000 son los puertos que generalmente se utlizan para tener un servido local en nustro equipo 
+
+// 3. Ejecutar el servidor en nuestro computador
+app.listen(port, ()=>{
+    console.log("El servidor se esta ejecutando coreectamente, en el puerto", port);
+});
---
+++ backend/src/models/users.model.js
@@ -0,0 +1,17 @@
+// 1. Importar las dependencias
+
+import mongoose from "mongoose";
+
+// 2. crearnos el esquema de datos 
+
+const userSchema = new mongoose.Schema({
+    userName: {type: String, required: true, unique: true},
+    fullName: {type: String, required: true},
+    email: {type: String, required: true, unique: true}, // unique -> que no se pueda repetir, que sea unico 
+    password: {type: String, required: true},
+    role: {type: String, default: "cliente"} // default -> que es un valor predeterminado 
+});
+
+// 3. Definir nuestro modelo 
+
+export const userModel = mongoose.model("user", userSchema);
