cebd22836160e1f9f20e7bcff92f03147ddf7f62
J0z3Hp <joseluismarinocuadros@gmail.com>
2024-11-22 21:17:42 -0500

    [update] agrego de rutas al app.js

--- backend/app.js
+++ backend/app.js
@@ -6,13 +6,14 @@ import { usersRouter } from "./src/routes/user.routes.js";
 import { loginUserRouter } from "./src/routes/loginUser.routes.js";
 import { adminRouter } from "./src/routes/admin.routes.js";
 import { orderRouter } from "./src/routes/orders.routes.js";
+import { loginAdminRouter } from "./src/routes/loginAdmin.routes.js";
  
 
 // Configurar el uso de nuestro servidor 
 const app = express();
 dotenv.config(); 
 connectionMongo (); 
-const port = process.env.PORT 
+const port = process.env.PORT  
 
 //RUTAS QUE DEBE UTILIZAR 
 app.use(express.json()); 
@@ -21,6 +22,7 @@ app.use("/usuarios", usersRouter);
 app.use("/iniciarSesion", loginUserRouter);
 app.use("/administradores", adminRouter);
 app.use("/ordenes", orderRouter); 
+app.use("/inicarSesionAdmin", loginAdminRouter);
 
 
 // Ejecutar el servidor en nuestro computador
--- backend/src/routes/orders.routes.js
+++ backend/src/routes/orders.routes.js
@@ -1,5 +1,6 @@
 import { createOrder, showOrderById, updatedOrder } from "../controllers/orders.controller.js";
 import express from "express";
+import { auth } from "../middleware/auth.js";
 
 export const orderRouter = express.Router();
 
