eed63c7600f103752e4a43140803479c392a6703
J0z3Hp <joseluismarinocuadros@gmail.com>
2024-11-29 20:59:18 -0500

    [create] organizacion del proyecto creada

---
+++ frontend/src/app/components/page-hombre/page-hombre.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-hombre',
+  standalone: true,
+  imports: [],
+  templateUrl: './page-hombre.component.html',
+  styleUrl: './page-hombre.component.css'
+})
+export class PageHombreComponent {
+
+}
---
+++ frontend/src/app/components/page-mujer/page-mujer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-mujer',
+  standalone: true,
+  imports: [],
+  templateUrl: './page-mujer.component.html',
+  styleUrl: './page-mujer.component.css'
+})
+export class PageMujerComponent {
+
+}
