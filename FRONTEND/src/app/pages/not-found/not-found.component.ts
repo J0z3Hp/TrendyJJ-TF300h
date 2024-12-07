6f9e341b0e99afef552b825eb70f29e9877e0d46
J0z3Hp <joseluismarinocuadros@gmail.com>
2024-12-07 02:23:28 -0500

    [update] page 404

--- FRONTEND/src/app/pages/not-found/not-found.component.ts
+++ FRONTEND/src/app/pages/not-found/not-found.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { NavBarComponent } from '../../components/nav-bar/nav-bar.component';
+import { FooterComponent } from '../../components/footer/footer.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-not-found',
   standalone: true,
-  imports: [],
+  imports: [NavBarComponent, FooterComponent, RouterLink],
   templateUrl: './not-found.component.html',
   styleUrl: './not-found.component.css'
 })
