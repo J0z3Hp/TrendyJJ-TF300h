5bc273a5aaf626ca56815b7a451a6f45ba2cc9a9
J0z3Hp <joseluismarinocuadros@gmail.com>
2024-12-06 22:20:51 -0500

    [update] pagina incio

--- FRONTEND/src/app/pages/inicio/inicio.component.ts
+++ FRONTEND/src/app/pages/inicio/inicio.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NavBarComponent } from '../../components/nav-bar/nav-bar.component';
 import { FooterComponent } from '../../components/footer/footer.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-inicio',
   standalone: true,
-  imports: [NavBarComponent, FooterComponent],
+  imports: [NavBarComponent, FooterComponent, RouterLink],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css'
 })
