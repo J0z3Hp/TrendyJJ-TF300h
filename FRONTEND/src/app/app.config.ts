0ddf4e9d8ced572ca04043420726f07492336c28
J0z3Hp <joseluismarinocuadros@gmail.com>
2024-12-10 21:30:43 -0500

    [update] config de app.config y creacion de formulario de registro usuarios

--- FRONTEND/src/app/app.config.ts
+++ FRONTEND/src/app/app.config.ts
@@ -1,8 +1,24 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
+import { provideToastr } from 'ngx-toastr';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: 
+  [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes),
+    provideHttpClient(),
+    provideToastr({
+      timeOut: 2000, 
+      positionClass: "toast-bottom-left", 
+      preventDuplicates: true, 
+      easeTime: 0, 
+      progressBar: false,
+    }),
+    provideAnimations(),
+  ]
 };
