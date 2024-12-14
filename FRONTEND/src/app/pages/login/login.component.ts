0898733c8c11e7cc78eae58f8ad5d9a5597f3c68
Juan F Rodriguez <juan.rodriguez9914@gmail.com>
2024-12-13 21:01:46 -0500

    Avance formularios de inicio de sesión

--- FRONTEND/src/app/pages/login/login.component.ts
+++ FRONTEND/src/app/pages/login/login.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import {ReactiveFormsModule, FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
+  formularioLoginUsers = new FormGroup({
+    email : new FormControl(''),
+    password: new FormControl('')
+  });
+
+  handleSubmint(){
+
+
+    console.log('Esta es la información obtenida del imput email' + this.formularioLoginUsers.value.email);
+    console.log('Esta es la información obtenida del imput password'+ this.formularioLoginUsers.value.password);
+  }
 }
