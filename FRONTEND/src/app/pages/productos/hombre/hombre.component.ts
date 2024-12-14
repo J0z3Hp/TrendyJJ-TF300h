0071ed6b4713c95e48c84421f749f99c712ba15c
J0z3Hp <joseluismarinocuadros@gmail.com>
2024-12-13 20:45:37 -0500

    [update] creación y finalización de los guards y los interceptors

--- FRONTEND/src/app/pages/productos/hombre/hombre.component.ts
+++ FRONTEND/src/app/pages/productos/hombre/hombre.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { PageHombreComponent } from '../../../components/page-hombre/page-hombre.component';
+import { NavBarComponent } from "../../../components/nav-bar/nav-bar.component";
+import { FooterComponent } from "../../../components/footer/footer.component";
 
 @Component({
   selector: 'app-hombre',
   standalone: true,
-  imports: [PageHombreComponent],
+  imports: [PageHombreComponent, NavBarComponent, FooterComponent],
   templateUrl: './hombre.component.html',
   styleUrl: './hombre.component.css'
 })
