ec0a79326c3ae3511ad8dd2ce4a852ac3a10fa5c
J0z3Hp <joseluismarinocuadros@gmail.com>
2024-12-13 22:18:30 -0500

    [update] mejora de component card mujer

--- FRONTEND/src/app/pages/productos/mujer/mujer.component.ts
+++ FRONTEND/src/app/pages/productos/mujer/mujer.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NavBarComponent } from '../../../components/nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-mujer',
   standalone: true,
-  imports: [],
+  imports: [NavBarComponent],
   templateUrl: './mujer.component.html',
   styleUrl: './mujer.component.css'
 })
