20e6c9d33991f6158b1b8704c04bd15c41f8f129
Juan F Rodriguez <juan.rodriguez9914@gmail.com>
2024-12-13 21:52:10 -0500

    Actualización formularios de inicio de sesión e inicio de peticiones de carta de productos

--- FRONTEND/src/app/admin/dash-board/login-admin/login-admin.component.ts
+++ FRONTEND/src/app/admin/dash-board/login-admin/login-admin.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import {ReactiveFormsModule, FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-login-admin',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login-admin.component.html',
   styleUrl: './login-admin.component.css'
 })
 export class LoginAdminComponent {
+
+  formularioLoginAdmin = new FormGroup({
+    email : new FormControl(''),
+    password: new FormControl('')
+  });
+
+  handleSubmint(){
+
+
+    console.log('Esta es la información obtenida del imput email' + this.formularioLoginAdmin.value.email);
+    console.log('Esta es la información obtenida del imput password'+ this.formularioLoginAdmin.value.password);
+  }
 
 }
